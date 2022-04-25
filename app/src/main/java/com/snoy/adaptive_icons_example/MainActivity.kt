2ece88bb027a9180c65915280ec70d3465f17454
Snoy Kuo <snoy.kuo@twprotech.com>
2022-04-25 11:09:15 +0800

    create project.

---
+++ app/src/main/java/com/snoy/adaptive_icons_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.snoy.adaptive_icons_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
